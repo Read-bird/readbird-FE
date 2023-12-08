a15aba8826629398a455f42571f1f70d1e836ca5
hyu-dev <hyu630115@gmail.com>
2023-12-08 12:53:31 +0900

    feat. template, pages 예시 파일 생성

---
+++ src/components/templates/HomeTemplate/HomeTemplate.tsx
@@ -0,0 +1,11 @@
+import { Images } from '@components/common/Images';
+
+type TProps = {};
+
+export const HomeTemplate = (props: TProps) => {
+	return (
+		<div>
+			<Images />
+		</div>
+	);
+};
---
+++ src/components/templates/PlanModalTemplate/PlanModalTemplate.tsx
@@ -0,0 +1,3 @@
+export const PlanModalTemplate = () => {
+	return <form>이곳에 모달안에 들어갈 플랜을 작성해보아요</form>;
+};
