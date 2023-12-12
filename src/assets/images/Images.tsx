6aea3285a324d03ce9301a421dfee0c3d0bc2262
hyu-dev <hyu630115@gmail.com>
2023-12-13 01:28:15 +0900

    modify. Images 컴포넌트 수정

--- src/assets/images/Images.tsx
+++ src/assets/images/Images.tsx
@@ -6,23 +6,18 @@ type TProps = {
   imgAlt?: string;
   imgWidth?: number;
   imgHeight?: number;
-  imgKey?: keyof typeof ImageKeys;
   imgStyle?: CSSObject;
 };
 
-const ImageKeys = {
-  no_image: '' // 여기에 import로 받아온 imgName 넣기 (로컬에서 관리할 img) - 없는 경우 제거
-};
-
-export const Images = memo(({ imgUrl, imgAlt, imgWidth, imgHeight, imgKey, imgStyle }: TProps) => {
+export const Images = memo(({ imgUrl, imgAlt, imgWidth, imgHeight, imgStyle }: TProps) => {
   const handleError = useCallback((e: SyntheticEvent<HTMLImageElement, Event>) => {
-    e.currentTarget.src = ImageKeys['no_image'];
+    e.currentTarget.src = require('/src/assets/images/noImage.jpeg');
   }, []);
 
   return (
     <ImageStyle
       imgStyle={imgStyle}
-      src={imgKey ? ImageKeys[imgKey] : imgUrl}
+      src={imgUrl}
       alt={imgAlt}
       width={imgWidth}
       height={imgHeight}
