7c3a884921c25216680bd63247d0cae84d0269c9
hyu-dev <hyu630115@gmail.com>
2023-12-12 16:54:09 +0900

    feat. className 합성 함수 생성

---
+++ src/utils/classname/function.ts
@@ -0,0 +1,24 @@
+// className 조합 함수
+type TClassArgs = (string | Record<string, boolean> | undefined)[];
+export const cls = (...classNames: TClassArgs) => {
+  let names: string[] = [];
+
+  for (let data of classNames) {
+    if (!data) {
+      continue;
+    }
+
+    if (typeof data === 'string') {
+      names.push(data);
+      continue;
+    }
+
+    for (let key in data) {
+      if (data[key]) {
+        names.push(key);
+      }
+    }
+  }
+
+  return names.join(' ');
+};
