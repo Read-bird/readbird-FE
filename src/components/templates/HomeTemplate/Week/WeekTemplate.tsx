d97266d59f63b6d469b9fd849a67fb9edf52b9a1
hyu-dev <hyu630115@gmail.com>
2023-12-14 01:23:01 +0900

    refactor. weekTemplate 컴포넌트 더미데이터 분리

--- src/components/templates/HomeTemplate/Week/WeekTemplate.tsx
+++ src/components/templates/HomeTemplate/Week/WeekTemplate.tsx
@@ -1,80 +1,10 @@
-import { EAchievementStatus, ERecordStatus, TPlanRecord } from '@api/types';
 import { IconPlus } from '@assets/icons';
 import { IconEggNoPlan, IconEggOnePlan, IconEggThreePlan, IconEggTwoPlan } from '@assets/images';
 import { CustomCalendar } from '@components/common/Calendar';
 import { Spacing } from '@components/common/Spacing';
 import { Plan } from '@components/templates/HomeTemplate/Plan';
+import { dummy } from '@mocks/index';
 import { AddPlanWrap, EmptyPlan, PlanListBox, PlanVisualBox, Wrap } from './Styled';
-
-const calendarDummy: TPlanRecord[] = [
-  {
-    createdAt: '2023-12-10',
-    status: EAchievementStatus.success
-  },
-  {
-    createdAt: '2023-12-11',
-    status: EAchievementStatus.failed
-  },
-  {
-    createdAt: '2023-12-12',
-    status: EAchievementStatus.unstable
-  },
-  {
-    createdAt: '2023-12-13',
-    status: null
-  },
-  {
-    createdAt: '2023-12-14',
-    status: null
-  },
-  {
-    createdAt: '2023-12-15',
-    status: null
-  },
-  {
-    createdAt: '2023-12-16',
-    status: null
-  }
-];
-
-const planDummy = [
-  {
-    planId: 1,
-    title: '나의 라임 오렌지 나무',
-    author: 'author',
-    coverImage: 'url',
-    totalPage: 100,
-    currentPage: 80,
-    target: 30,
-    endDate: '2023-12-21',
-    planStatus: ERecordStatus.inProgress,
-    recordStatus: ERecordStatus.inProgress
-  },
-  {
-    planId: 2,
-    title: '다빈치코드',
-    author: 'author',
-    coverImage: 'url',
-    totalPage: 100,
-    currentPage: 30,
-    target: 10,
-    endDate: '2023-12-15',
-    planStatus: ERecordStatus.inProgress,
-    recordStatus: ERecordStatus.inProgress
-  },
-  {
-    planId: 3,
-    title: '천사와 악마',
-    author: 'author',
-    coverImage: 'url',
-    totalPage: 100,
-    currentPage: 1,
-    target: 30,
-    endDate: '2023-12-30',
-    planStatus: ERecordStatus.inProgress,
-    recordStatus: ERecordStatus.inProgress
-  }
-];
 
 export const WeekTemplate = () => {
   const handleClickAdd = () => {
@@ -83,7 +13,7 @@ export const WeekTemplate = () => {
 
   return (
     <Wrap>
-      <CustomCalendar record={calendarDummy} />
+      <CustomCalendar record={dummy.weekCalendar} />
       <Spacing height={20} />
       <PlanVisualBox>
         {
@@ -92,13 +22,13 @@ export const WeekTemplate = () => {
             1: <IconEggOnePlan />,
             2: <IconEggTwoPlan />,
             3: <IconEggThreePlan />
-          }[planDummy?.length ?? 0]
+          }[dummy.plan?.length ?? 0]
         }
       </PlanVisualBox>
       <Spacing height={20} />
-      {!!planDummy?.length ? (
+      {!!dummy.plan?.length ? (
         <PlanListBox>
-          {planDummy.map((plan) => (
+          {dummy.plan.map((plan) => (
             <li key={plan.planId}>
               <Plan {...plan} />
             </li>
@@ -108,7 +38,7 @@ export const WeekTemplate = () => {
         <EmptyPlan>아직 읽고 있는 책이 없어요.</EmptyPlan>
       )}
       {/* 총 등록개수가 3개가 되면 버튼 숨김 */}
-      {(planDummy?.length ?? 0) < 3 && (
+      {(dummy.plan?.length ?? 0) < 3 && (
         <AddPlanWrap onClick={handleClickAdd}>
           <IconPlus />
         </AddPlanWrap>
