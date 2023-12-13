dbb487c4334aae46168ad6c8f14189b33d7e1a21
hyu-dev <hyu630115@gmail.com>
2023-12-14 01:24:39 +0900

    modify. 캘린더 스타일 적용, 캘린더 적용

--- src/components/common/Calendar/Calendar.tsx
+++ src/components/common/Calendar/Calendar.tsx
@@ -1,25 +1,58 @@
-import { useState } from 'react';
-import ReactCalendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import { EAchievementStatus, TPlanRecord } from '@api/types';
+import { DayBird } from '@components/common/DayBird';
+import { cls, getClassByStatus } from '@utils/classname';
+import dayjs from 'dayjs';
+import { MouseEvent, useCallback, useState } from 'react';
+import ReactCalendar, { TileContentFunc, TileDisabledFunc } from 'react-calendar';
+import '../../../styles/calendar.css';
 
 type ValuePiece = Date | null;
-
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-export const Calendar = () => {
+type TProps = {
+  record: TPlanRecord[];
+};
+
+export const Calendar = ({ record }: TProps) => {
   const [value, setValue] = useState<Value>(new Date());
 
-  const onChange = (value: Value, event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const onChange = (value: Value, event: MouseEvent<HTMLButtonElement>) => {
     console.log(value);
   };
 
+  const tileContent: TileContentFunc = useCallback(
+    ({ date }) => {
+      const className = getClassByStatus(date, EAchievementStatus.success);
+
+      return (
+        <DayBird
+          key={`${date}`}
+          className={cls(className)}
+          cursor={className === 'after-today' ? 'default' : 'pointer'}
+        >
+          <p className={cls(className)}>{dayjs(date).format('DD')}</p>
+        </DayBird>
+      );
+    },
+    [record]
+  );
+
+  const tileDisabled: TileDisabledFunc = useCallback(
+    ({ date }) => getClassByStatus(date, null) === 'after-today',
+    []
+  );
+
   return (
-    <div className="Calendar">
-      <div className="Calendar__container">
-        <main className="Calendar__container__content">
-          <ReactCalendar onChange={onChange} value={value} />
-        </main>
-      </div>
-    </div>
+    <ReactCalendar
+      value={value}
+      onClickDay={onChange}
+      calendarType="gregory"
+      formatDay={(_, date) => dayjs(date).format('D')}
+      goToRangeStartOnSelect={false}
+      maxDate={new Date(dayjs().add(1, 'year').format('YYYY-MM-DD'))}
+      showNavigation={false}
+      tileContent={tileContent}
+      tileDisabled={tileDisabled}
+    />
   );
 };
