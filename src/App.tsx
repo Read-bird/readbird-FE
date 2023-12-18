501e51208a380027cc24c9539db356bd3661586a
hyu-dev <hyu630115@gmail.com>
2023-12-18 16:18:47 +0900

    chore. App 프리티어로 인한 코드 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import {setCurrentDate} from '@/store/reducers';
-import {TAppDispatch} from '@/store/state';
-import {LoadingTemplate} from '@components/templates/LoadingTemplate';
-import {appRouter} from '@routers/appRouter';
-import {GlobalStyle, theme} from '@style/global-style';
+import { setCurrentDate } from '@/store/reducers';
+import { TAppDispatch } from '@/store/state';
+import { LoadingTemplate } from '@components/templates/LoadingTemplate';
+import { appRouter } from '@routers/appRouter';
+import { GlobalStyle, theme } from '@style/global-style';
 import dayjs from 'dayjs';
-import {useEffect} from 'react';
-import {useDispatch} from 'react-redux';
-import {RouterProvider, useNavigate} from 'react-router-dom';
-import {ThemeProvider} from 'styled-components';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 export const App = () => {
+  // 첫 접속시 날짜 등록
+  const dispatch = useDispatch<TAppDispatch>();
 
-    // 첫 접속시 날짜 등록
-    const dispatch = useDispatch<TAppDispatch>();
+  useEffect(() => {
+    // 첫 접속시 당일 날짜 등록
+    dispatch(setCurrentDate(dayjs().format()));
+  }, []);
 
-    useEffect(() => {
-        // 첫 접속시 당일 날짜 등록
-        dispatch(setCurrentDate(dayjs().format()));
-    }, []);
-
-    return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyle/>
-            <RouterProvider router={appRouter} fallbackElement={<LoadingTemplate/>}/>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <RouterProvider router={appRouter} fallbackElement={<LoadingTemplate />} />
+    </ThemeProvider>
+  );
 };
