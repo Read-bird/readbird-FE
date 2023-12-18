8cb4c5a0a466523ee156cc9f6a163e986b0b2553
hyu-dev <hyu630115@gmail.com>
2023-12-19 04:33:00 +0900

    feat. plan api 조회, 삭제 로직 추가

---
+++ src/api/plan.ts
@@ -0,0 +1,34 @@
+import { authFetch } from '@api/axios';
+import { TPlanData } from '@api/types';
+import { AxiosError } from 'axios';
+
+type TResponse<T> = { error: boolean; success: boolean; data: T };
+
+// 플랜 조회하기
+export const getPlanList = async (date: string): Promise<TResponse<TPlanData | string>> => {
+  try {
+    const result = await authFetch.get('/api/plan', { params: { date } });
+    return { error: false, success: true, data: result.data };
+  } catch (e) {
+    if (e instanceof AxiosError) {
+      return { error: true, success: false, data: e.response?.data.message ?? '' };
+    }
+
+    return { error: true, success: false, data: '' };
+  }
+};
+
+// 플랜 삭제하기
+export const deletePlan = async (planId: number, userId: number): Promise<TResponse<string>> => {
+  try {
+    const result = await authFetch.delete(`/api/plan/${planId}`, { data: { userId } });
+
+    return { error: false, success: true, data: result.data };
+  } catch (e: any) {
+    if (e instanceof AxiosError) {
+      return { error: true, success: false, data: e.response?.data.message ?? '' };
+    }
+
+    return { error: true, success: false, data: '' };
+  }
+};
