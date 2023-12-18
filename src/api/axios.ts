340c9efc1fd5dc4337cedaa1fabc396c23483ba5
hyu-dev <hyu630115@gmail.com>
2023-12-19 04:32:42 +0900

    add. axios refreshtoken 갱신 로직 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,17 +1,33 @@
 import axios from 'axios';
 
 export const authFetch = axios.create({
-  baseURL: `${process.env.REACT_APP_SERVER_PATH}`
+  baseURL: `${process.env.REACT_APP_SERVER_PATH}`,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  }
 });
 
 authFetch.interceptors.request.use(
   (config) => {
-    config.headers['Content-Type'] = 'application/json';
-    config.headers['Accept'] = 'application/json';
     config.headers.Authorization = `${localStorage.getItem('rb-access-token')}`;
+    config.headers.refreshtoken = `${localStorage.getItem('rb-refresh-token')}`;
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+authFetch.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const status = error.response.status;
+    if (status === 412) {
+      const result = await authFetch.post('/api/user/token');
+      localStorage.setItem('rb-access-token', result.headers?.authorization);
+    }
+
     return Promise.reject(error);
   }
 );
