8ef5e1570f78062cd2da0a736ca7dae32f56a49b
hyu-dev <hyu630115@gmail.com>
2023-12-20 22:18:57 +0900

    refactor: axios interceptor hooks 이동

---
+++ src/hooks/axiosInterceptor.ts
@@ -0,0 +1,82 @@
+import { setLoading } from '@/store/reducers';
+import { TAppDispatch } from '@/store/state';
+import { authFetch } from '@api/axios';
+import { Alert } from '@utils/Alert';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+export const useAxiosInterceptor = () => {
+  const dispatch = useDispatch<TAppDispatch>();
+
+  useEffect(() => {
+    authFetch.interceptors.request.use(
+      (config) => {
+        dispatch(setLoading(true));
+        config.headers.Authorization = `${localStorage.getItem('rb-access-token')}`;
+        config.headers.RefreshToken = `${localStorage.getItem('rb-refresh-token')}`;
+        return config;
+      },
+      (error) => {
+        dispatch(setLoading(true));
+        Promise.reject(error);
+      }
+    );
+
+    authFetch.interceptors.response.use(
+      (response) => {
+        dispatch(setLoading(false));
+        return response;
+      },
+      async (error) => {
+        const {
+          config,
+          response: { status, data: message }
+        } = error;
+
+        dispatch(setLoading(false));
+
+        // 토큰이 만료되을 때
+        if (status === 412) {
+          if (message.includes('토큰 갱신')) {
+            const response = await authFetch.post('/api/user/token', {
+              RefreshToken: localStorage.getItem('rb-refresh-token')
+            });
+            // 리프레시 토큰 요청이 성공할 때
+            if (response.status === 200) {
+              const authorization = response.headers?.authorization;
+              localStorage.setItem('rb-access-token', authorization);
+              axios.defaults.headers.common.Authorization = `Bearer ${authorization}`;
+              // 진행중이던 요청 이어서하기
+              const originRequest = config;
+              originRequest.headers.Authorization = `Bearer ${authorization}`;
+              return axios(originRequest);
+            }
+          }
+          // 토큰과 리프레시 토큰이 모두 만료되어 로그인이 필요할 떄
+          else if (message.includes('로그인')) {
+            Alert.error({
+              title: '로그인이 필요합니다.',
+              action: () => {
+                localStorage.clear();
+                window.location.replace('/login');
+              }
+            });
+          }
+        }
+        // 유저 토큰이 유효하지 않을 떄
+        else if (status === 401) {
+          Alert.error({
+            title: '유저정보가 올바르지 않습니다.',
+            action: () => {
+              localStorage.clear();
+              window.location.replace('/login');
+            }
+          });
+        }
+
+        return Promise.reject(error);
+      }
+    );
+  }, []);
+};
