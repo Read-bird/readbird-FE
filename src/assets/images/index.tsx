afa831cbc1cb87debeb905f9b7c940086971e4a9
hyu-dev <hyu630115@gmail.com>
2023-12-20 22:21:39 +0900

    resource. error, loading bird svg 추가

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,5 +1,7 @@
+export * from './ErrorBird';
 export * from './IconEggNoPlan';
 export * from './IconEggOnePlan';
 export * from './IconEggThreePlan';
 export * from './IconEggTwoPlan';
 export * from './Images';
+export * from './LoadingBird';
