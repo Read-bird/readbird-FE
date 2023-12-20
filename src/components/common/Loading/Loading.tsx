00a77c61973e7524a376aa6af42162711027ed91
hyu-dev <hyu630115@gmail.com>
2023-12-20 22:19:17 +0900

    feat. 로딩 컴포넌트 퍼블리싱

--- src/components/common/Loading/Loading.tsx
+++ src/components/common/Loading/Loading.tsx
@@ -1,3 +1,62 @@
+import { LoadingBird } from '@assets/images';
+import { Dots } from '@components/common/Loading/Dots';
+import { Spacing } from '@components/common/Spacing';
+import styled from 'styled-components';
+
 export const Loading = () => {
-  return <div>로딩중이에요.</div>;
+  return (
+    <Wrap>
+      <ImageWrap>
+        <LoadingBird />
+      </ImageWrap>
+      <Spacing height={20} />
+      <FlexBox>
+        <Text>책장을 넘기는 중</Text>
+        <Dots />
+      </FlexBox>
+    </Wrap>
+  );
 };
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ImageWrap = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  border: 2px solid ${({ theme }) => theme.colors.basicDark};
+  background-color: ${({ theme }) => theme.colors.basic};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FlexBox = styled.div`
+  position: relative;
+  width: 200px;
+
+  .absolute {
+    position: absolute;
+    bottom: 0;
+    left: 180px;
+    font-size: 24px;
+    font-weight: 400;
+    color: #747474;
+    line-height: 24px;
+    text-align: center;
+  }
+`;
+
+const Text = styled.p`
+  font-size: 24px;
+  font-weight: 400;
+  color: #747474;
+  line-height: 24px;
+  text-align: center;
+`;
