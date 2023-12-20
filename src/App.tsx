d0bd5a9b86212a7ed4a33a4e689def3e53b4c5be
hyu-dev <hyu630115@gmail.com>
2023-12-20 22:20:34 +0900

    modify. api 호출로딩, router fallback 로딩 구분 App 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { setCurrentDate } from '@/store/reducers';
 import { TAppDispatch } from '@/store/state';
-import { LoadingTemplate } from '@components/templates/LoadingTemplate';
+import { LoadingTemplate, RouterLoading } from '@components/templates/LoadingTemplate';
+import { useAxiosInterceptor } from '@hooks/axiosInterceptor';
 import { appRouter } from '@routers/appRouter';
 import { GlobalStyle, theme } from '@style/global-style';
 import dayjs from 'dayjs';
@@ -12,6 +13,8 @@ import { ThemeProvider } from 'styled-components';
 export const App = () => {
   // 첫 접속시 날짜 등록
   const dispatch = useDispatch<TAppDispatch>();
+  // axios interceptor
+  useAxiosInterceptor();
 
   useEffect(() => {
     // 첫 접속시 당일 날짜 등록
@@ -21,7 +24,8 @@ export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <RouterProvider router={appRouter} fallbackElement={<LoadingTemplate />} />
+      <LoadingTemplate />
+      <RouterProvider router={appRouter} fallbackElement={<RouterLoading />} />
     </ThemeProvider>
   );
 };
