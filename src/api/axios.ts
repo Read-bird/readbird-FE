f71e7926875c06a2e10ad15c6de5fb8a77d10bda
hyu-dev <hyu630115@gmail.com>
2023-12-22 17:04:42 +0900

    add. axiosFetch 함수 생성
    - axios 일괄 호출 위한 함수 적용

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig, Method } from 'axios';
 
 export const authFetch = axios.create({
   baseURL: `${process.env.REACT_APP_SERVER_PATH}`,
@@ -7,3 +7,25 @@ export const authFetch = axios.create({
     Accept: 'application/json'
   }
 });
+
+type TAxiosFetch<T extends unknown> = {
+  method: Method;
+  url: string;
+  options?: AxiosRequestConfig<T>;
+  errorAction?: (e: any) => void;
+};
+
+// authFetch 일괄적으로 사용하기 위해 생성한 fetch 함수
+// 제네릭 타입 T는 options에 있는 data의 타입을 말함
+export const axiosFetch = async <T = any>({
+  method,
+  url,
+  options,
+  errorAction
+}: TAxiosFetch<T>) => {
+  try {
+    return await authFetch({ url, method, ...options });
+  } catch (e) {
+    errorAction?.(e);
+  }
+};
