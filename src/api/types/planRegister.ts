198f0154b8a2e7d09339ee130ac75e999d39f5a5
hyu-dev <hyu630115@gmail.com>
2023-12-27 00:57:39 +0900

    add. TRegisterFormValue planRegister type 추가

--- src/api/types/planRegister.ts
+++ src/api/types/planRegister.ts
@@ -1,10 +1,29 @@
+import { TBook } from '@/store/reducers';
+
 export interface IRegisterForm {
-    planId: number | null;
-    title: string | null;
-    author: string | null;
-    publisher: string | null
-    totalPage: string | number | null;
-    currentPage: string;
-    startDate: string;
-    endDate: string;
+  planId: number | null;
+  title: string | null;
+  author: string | null;
+  publisher: string | null;
+  totalPage: string | number | null;
+  currentPage: string;
+  startDate: string;
+  endDate: string;
 }
+
+export type TRegisterFormValue = {
+  planId: number | null;
+  title: string | null;
+  author: string | null;
+  publisher: string | null;
+  totalPage: number;
+  currentPage: number;
+  startDate: string;
+  endDate: string;
+  bookId: number | null;
+  searchData: {
+    bookList: TBook[];
+    page: number;
+    totalPage: number;
+  };
+};
