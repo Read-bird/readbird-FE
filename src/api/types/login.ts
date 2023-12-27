148243dc987cd4d25abfdf77f491b8ea23b0e4e3
hyu-dev <hyu630115@gmail.com>
2023-12-28 00:16:06 +0900

    modify. login api type 추가

--- src/api/types/login.ts
+++ src/api/types/login.ts
@@ -1,8 +1,15 @@
 export type TLoginResType = {
-    userId: number,
-    email: string,
-    nickname: string,
-    imageUrl: string,
-    accessToken: string,
-    refreshToken: string
-}
+  userId: number;
+  email: string;
+  nickname: string;
+  imageUrl: string;
+  accessToken: string;
+  refreshToken: string;
+  character?: {
+    characterId: number;
+    name: string;
+    content: string;
+    imageUrl: string;
+    getDate: string;
+  };
+};
