38b4473a226180a8d764939cc36e13d4d093d5b3
hyu-dev <hyu630115@gmail.com>
2023-12-28 22:09:44 +0900

    modify. 배너 링크 env 설정

--- src/components/templates/MyPage/MyMainTemplate/MyBanner.tsx
+++ src/components/templates/MyPage/MyMainTemplate/MyBanner.tsx
@@ -2,18 +2,12 @@ import styled from 'styled-components';
 
 export const MyBanner = () => {
   const handleClick = () => {
-    window.open(
-      'https://docs.google.com/forms/d/e/1FAIpQLSemXs84Y7kMJ8eRB7qLrcJdFKeHobaGYFgup3MQruE_PVnJDg/viewform',
-      '_blank'
-    );
+    window.open(process.env.REACT_APP_EVENT_URL, '_blank');
   };
 
   return (
     <StyledBanner onClick={handleClick}>
-      <img
-        src={'https://readbird2.s3.ap-northeast-2.amazonaws.com/readbird/google_form_banner.png'}
-        alt="배너"
-      />
+      <img src={process.env.REACT_APP_EVENT_BANNER} alt="배너" />
     </StyledBanner>
   );
 };
